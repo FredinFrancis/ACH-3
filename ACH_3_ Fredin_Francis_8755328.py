b5a3edfdad3d2c1737797a847d29191e4f10aa46
FredinFrancis <fredinfrancis010@gmail.com>
2021-10-04 21:27:55 +0530

    first commit

---
+++ ACH_3_ Fredin_Francis_8755328.py
@@ -0,0 +1,13 @@
+print("\n")
+print("Program to Print Squares of Even Numbers by accepting number from User !")
+print("************************************************************************")
+num=int(input("Enter a number -"))
+f=0
+result=0
+while f <= num:
+    result+=f**2
+    f +=2
+print(result)
+ 
+        
+        
